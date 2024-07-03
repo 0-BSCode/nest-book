fa4fc85eb5f73e57c623fab7195ff5ab2d0d1a1b
0-BSCode <bryanipsanchez@gmail.com>
2024-07-03 15:37:57 +0800

    Custom filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { AuthorsModule } from './modules/authors/authors.module';
 import { BooksModule } from './modules/books/books.module';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from './filters/http-exception/http-exception.filter';
 
 @Module({
   imports: [
@@ -25,6 +27,12 @@ import { BooksModule } from './modules/books/books.module';
     BooksModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
