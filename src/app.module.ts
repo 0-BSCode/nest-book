ff26eb3fe2f43b84991200ee14ecaccdcab16af7
0-BSCode <bryanipsanchez@gmail.com>
2024-07-04 23:37:44 +0800

    Docker configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { config } from './config/config';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: 'src/env/.env',
+      envFilePath: '.env',
       load: [config],
       isGlobal: true,
     }),
